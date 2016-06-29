58adb5f58ddd50a6a216bb9501770d5d939cc004
vasavivangipurapu <vrkrao7@gmail.com>
2016-06-29 17:02:10 +0530

    triloks program edited

--- src/com/mss/ReadPropertiesFile.java
+++ src/com/mss/ReadPropertiesFile.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
  * @author miracle
  */
 public class ReadPropertiesFile {
-
+//this is triloks program and we are commiting
     public void readPropertiesFile() {
         Properties p = new Properties();
         InputStream iStream = null;
